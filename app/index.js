90cd6394363311853065e6005b2f5533e9951f11
jkolivuo <jkolivuo@gmail.com>
2018-08-10 22:02:35 +0300

    first version

--- app/index.js
+++ app/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Navigator from './config/routes';
-import store from './config/store';
+import configureStore from './config/store';
+import rootSaga from './sagas';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
   $white: '#FFFF',
   $black: '#FF000000',
 });
+
+const store = configureStore();
+store.runSaga(rootSaga);
 
 export default () => (
   <Provider store={store}>
